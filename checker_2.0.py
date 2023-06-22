07b1004bab924e5b0999f42bba017c1b76be2737
PredyDaddy <1642942273@qq.com>
2023-06-22 16:38:45 +0800

    Update checker_2.0.py

--- checker_2.0.py
+++ checker_2.0.py
@@ -23,65 +23,30 @@ KING_VALUE = 20  # 升王棋子的价值
 EDGE_VALUE = 5  # 边缘棋子的价值
 CENTER_VALUE = 3  # 中心棋子的价值
 
-# Draw the checkerboard
 def draw_board():
     # Clear the canvas
     canvas.delete("all")
-
     # Draw the squares of the checkerboard
     for row in range(8):
         for col in range(8):
             # Calculate the coordinates of the square based on the row and column numbers
             x1, y1 = col * 60, row * 60
             x2, y2 = x1 + 60, y1 + 60
-
             # Fill the square with gray if it's a black square
             if (row + col) % 2 == 0:
                 canvas.create_rectangle(x1, y1, x2, y2, fill="#8B4513")
-
             # If there's a piece on the square, draw the piece with its color and shape
             piece = board[row][col]
             if piece is not None:
                 # Draw a circle with the color of the piece
-                color = "white" if piece == "white" else "black" if piece == "black" else "green" if piece == "green" else "yellow"
+                color = "white" if piece == "white" else "black" if piece == "black" else "blue" if piece == "green" else "red"
                 canvas.create_oval(x1 + 5, y1 + 5, x2 - 5, y2 - 5, fill=color)
-
-                # If the piece is a king, draw a star on the circle
-                if piece in ("green", "yellow"):
-                    canvas.create_oval(
-                        x1 + 10, y1 + 10,   # Top-left coordinate of the oval
-                        x2 - 10, y2 - 10,   # Bottom-right coordinate of the oval
-                        fill="blue"
-                    )
-
-                    canvas.create_oval(
-                        x1 + 20, y1 + 20,   # Top-left coordinate of the eye on the left
-                        x1 + 30, y1 + 30,   # Bottom-right coordinate of the eye on the left
-                        fill="black"
-                    )
-
-                    canvas.create_oval(
-                        x2 - 30, y1 + 20,   # Top-left coordinate of the eye on the right
-                        x2 - 20, y1 + 30,   # Bottom-right coordinate of the eye on the right
-                        fill="black"
-                    )
-
-                    canvas.create_arc(
-                        x1 + 20, y1 + 30,   # Top-left coordinate of the arc
-                        x2 - 20, y2 - 10,   # Bottom-right coordinate of the arc
-                        start=200,          # Start angle of the arc
-                        extent=140,         # Extent angle of the arc
-                        style="arc",
-                        width=2
-                    )
-
-
             # If the square is a selected piece or a valid move, mark it with a yellow border
             if selected_piece is not None and (row, col) in get_valid_moves(board, selected_piece[0], selected_piece[1]):
                 canvas.create_rectangle(x1 + 2, y1 + 2, x2 - 2, y2 - 2, outline="black", width=4)
-
     # Update the canvas
     canvas.update()
+
 
 def is_valid_move(game_board, start_row, start_col, end_row, end_col):
     piece_color = game_board[start_row][start_col]
